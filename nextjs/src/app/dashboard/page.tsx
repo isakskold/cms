f489b9e674f89957633ba091bf7a097b2c58d5fd
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2025-02-08 14:47:01 +0100

    Cleanup and initial setup

---
+++ nextjs/src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+interface Props {}
+
+const page: React.FC<Props> = () => {
+  return (
+    <div className="h-full flex flex-col items-center">
+      <h1 className="text-3xl font-semibold">Welcome to your dashboard</h1>
+    </div>
+  );
+};
+
+export default page;
