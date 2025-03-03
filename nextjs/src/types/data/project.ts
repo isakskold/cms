69adc4f86463cf442d6ed3ddd34d6ba77c060242
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2025-03-03 21:04:27 +0100

    update type

--- nextjs/src/types/data/project.ts
+++ nextjs/src/types/data/project.ts
@@ -4,7 +4,7 @@ export interface Project {
   name: string;
   logo: string;
   description: string;
-  longDescription: string[];
+  longDescription: string;
   skills: string[];
   website: string;
   github: string;
