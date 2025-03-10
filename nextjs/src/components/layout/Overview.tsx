367dfbe1ee92da29c584b2d33488b101b918dfe0
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2025-03-10 15:34:36 +0100

    Async loading component

--- nextjs/src/components/layout/Overview.tsx
+++ nextjs/src/components/layout/Overview.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import useProjectStore from "@/stores/project/useProjectStore";
 import { useAuthStore } from "@/stores/auth/useAuthStore";
 import fetchProjects from "@/requests/project/fetchProjects";
+import Loading from "../utils/loading";
 
 interface Props {}
 
@@ -14,17 +15,23 @@ const Overview: React.FC<Props> = () => {
   const { access_token } = useAuthStore().tokenData || {};
   const { isHydrated } = useAuthStore();
 
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     if (!isHydrated) return;
 
+    // Only fetch from database if zustand state is empty
     if (projects.length === 0) {
       (async () => {
         try {
+          setLoading(true);
           const response = await fetchProjects(access_token as string);
           setProjects(response);
           console.log("Initialized store with fetched data");
         } catch (error) {
           console.error("Error fetching projects:", error);
+        } finally {
+          setLoading(false); // Ensure loading is set to false regardless of success or failure
         }
       })();
     }
@@ -32,28 +39,32 @@ const Overview: React.FC<Props> = () => {
 
   return (
     <div className="overflow-x-auto">
-      <table className="w-full border-collapse border border-gray-300">
-        <thead>
-          <tr className="bg-gray-100">
-            <th className="border p-2">ID</th>
-            <th className="border p-2">Project Name</th>
-            <th className="border p-2">Last edited</th>
-          </tr>
-        </thead>
-        <tbody>
-          {projects.map((project) => (
-            <tr
-              key={project.id}
-              onClick={() => router.push(`/project/${project.id}`)}
-              className="hover:bg-gray-50 cursor-pointer"
-            >
-              <td className="border p-2">{project.id}</td>
-              <td className="border p-2">{project.name}</td>
-              <td className="border p-2">{project.lastEdited || "Never"}</td>
+      {loading ? (
+        <Loading text="Loading projects..." />
+      ) : (
+        <table className="w-full border-collapse border border-gray-300">
+          <thead>
+            <tr className="bg-gray-100">
+              <th className="border p-2">ID</th>
+              <th className="border p-2">Project Name</th>
+              <th className="border p-2">Last edited</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {projects.map((project) => (
+              <tr
+                key={project.id}
+                onClick={() => router.push(`/project/${project.id}`)}
+                className="hover:bg-gray-50 cursor-pointer"
+              >
+                <td className="border p-2">{project.id}</td>
+                <td className="border p-2">{project.name}</td>
+                <td className="border p-2">{project.lastEdited || "Never"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
